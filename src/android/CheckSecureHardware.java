fc39762bf0b0eae22a6a272daab4a3de6e12b757
ArthurLe <arthur_leung@ultimatesoftware.com>
2017-08-12 20:07:02 -0400

    Fixed test key not being the correct type (was EC, should be RSA to match what the secure storage plugin uses)

--- src/android/CheckSecureHardware.java
+++ src/android/CheckSecureHardware.java
@@ -2,6 +2,8 @@ package com.arthurle.plugins;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,10 +26,21 @@ import android.security.KeyChain;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyInfo;
 import android.security.keystore.KeyProperties;
+import android.hardware.fingerprint.FingerprintManager;
+import android.util.Log;
+
 
 public class CheckSecureHardware extends CordovaPlugin {
 
-  private String messsage;
+  private String message;
+    private FingerprintManager mFingerPrintManager;
+
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        mFingerPrintManager = cordova.getActivity().getApplicationContext()
+                .getSystemService(FingerprintManager.class);
+
+    }
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -37,7 +50,7 @@ public class CheckSecureHardware extends CordovaPlugin {
         callbackContext.success();
         return true;
       } else {
-        callbackContext.error("Secure hardware not available, " + messsage);
+        callbackContext.error("[DBG] Secure hardware not available, error was " + message);
         return false;
       }
     }
@@ -48,40 +61,47 @@ public class CheckSecureHardware extends CordovaPlugin {
   //Requires at least Marshmallow (API 23)
   @TargetApi(Build.VERSION_CODES.M)
   private boolean checkSecureHardware() {
+    message += " using non-legacy method";
     KeyInfo keyInfo = null;
-    // Generate dummy EC key
-    // NoSuchAlgorithmException if EC unsupported
+      boolean authRequired = false;
     try{
+      // Only for devices with fingerprint reader
+        if (mFingerPrintManager.isHardwareDetected() && mFingerPrintManager.hasEnrolledFingerprints()){
+            authRequired = true
+        }
       KeyPairGenerator kpg = KeyPairGenerator.getInstance(
-        KeyProperties.KEY_ALGORITHM_EC,"AndroidKeyStore");
+        KeyProperties.KEY_ALGORITHM_RSA,"AndroidKeyStore");
       kpg.initialize(new KeyGenParameterSpec.Builder(
         "checkSecureHardware",
         KeyProperties.PURPOSE_SIGN|KeyProperties.PURPOSE_VERIFY)
         .setDigests(KeyProperties.DIGEST_SHA256,
           KeyProperties.DIGEST_SHA512)
-        .build());
-
+              .setUserAuthenticationRequired(authRequired)
+              .build());
       KeyPair kp = kpg.generateKeyPair();
       KeyFactory factory = KeyFactory.getInstance(kp.getPrivate().getAlgorithm(), "AndroidKeyStore");
 
       // Find if key is actually in secure hardware
-      // InvalidKeySpecException
       keyInfo = factory.getKeySpec(kp.getPrivate(),KeyInfo.class);
     }
-    catch (InvalidKeySpecException | InvalidAlgorithmParameterException | NoSuchProviderException | NoSuchAlgorithmException e) {
-      messsage = "Failed to generate dummy key or get dummy key info";
+    catch (Exception e) {
+      message = Log.getStackTraceString(e);
     }
     if (keyInfo != null){
       try {
         // Either condition is true when HW keystore is avaiblable
         boolean result = (keyInfo.isUserAuthenticationRequirementEnforcedBySecureHardware() || keyInfo.isInsideSecureHardware());
+//        System.out.println("[DBG] keyInfo.isUserAuthenticationRequirementEnforcedBySecureHardware() " + keyInfo.isUserAuthenticationRequirementEnforcedBySecureHardware());
+//        System.out.println("[DBG] keyInfo.isInsideSecureHardware() "+keyInfo.isInsideSecureHardware());
+//        System.out.println("[DBG] KeyChain.isBoundKeyAlgorithm(KeyProperties.KEY_ALGORITHM_EC) "+KeyChain.isBoundKeyAlgorithm(KeyProperties.KEY_ALGORITHM_EC));
+//        System.out.println("[DBG] KeyChain.isBoundKeyAlgorithm(KeyProperties.KEY_ALGORITHM_RSA) "+KeyChain.isBoundKeyAlgorithm(KeyProperties.KEY_ALGORITHM_RSA));
         KeyStore store = KeyStore.getInstance("AndroidKeyStore");
         store.load(null, null);
         store.deleteEntry("checkSecureHardware");
         // Delete the key after with the alias used
         return result;
-      } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException |IOException e) {
-        messsage = "Failed to delete dummy key";
+      } catch (Exception e) {
+        message = Log.getStackTraceString(e);
       }
     }
     return false;
@@ -90,7 +110,8 @@ public class CheckSecureHardware extends CordovaPlugin {
   //Fallback to legacy/ JellyBean implementation (API 18)
   @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
   private boolean checkSecureHardwareLegacy(){
+    message += " using legacy method";
     // Deprecated for >= API 23
-    return (KeyChain.isBoundKeyAlgorithm(KeyProperties.KEY_ALGORITHM_EC) && KeyChain.isBoundKeyAlgorithm(KeyProperties.KEY_ALGORITHM_RSA));
+    return (KeyChain.isBoundKeyAlgorithm(KeyProperties.KEY_ALGORITHM_RSA));
   }
 }
