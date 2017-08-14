0e88f6cf024ea239bc33b5d6c947460cee9ea86e
ArthurLe <arthur_leung@ultimatesoftware.com>
2017-08-14 15:15:33 -0400

    - Fixed plugin to check for both RSA and EC key storage if hardware backing is present

--- src/android/CheckSecureHardware.java
+++ src/android/CheckSecureHardware.java
@@ -2,8 +2,6 @@ package com.arthurle.plugins;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
-import org.apache.cordova.CordovaWebView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,92 +24,87 @@ import android.security.KeyChain;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyInfo;
 import android.security.keystore.KeyProperties;
-import android.hardware.fingerprint.FingerprintManager;
-import android.util.Log;
-
 
 public class CheckSecureHardware extends CordovaPlugin {
 
-  private String message;
-    private FingerprintManager mFingerPrintManager;
+  private final String dummyKeyNamespace = "checkSecureHardware";
+  private final String storeTarget = "AndroidKeyStore";
 
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        super.initialize(cordova, webView);
-        mFingerPrintManager = cordova.getActivity().getApplicationContext()
-                .getSystemService(FingerprintManager.class);
-
-    }
+  private String keyErrorMessage;
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    if (action.equals("checkSecureHardware")) {
+
+    if (action.equals(dummyKeyNamespace)) {
       boolean hasHardware = (Build.VERSION.SDK_INT >= 23) ? this.checkSecureHardware() : this.checkSecureHardwareLegacy();
+
       if (hasHardware){
         callbackContext.success();
         return true;
       } else {
-        callbackContext.error("[DBG] Secure hardware not available, error was " + message);
+        callbackContext.error("Secure hardware not available, " + keyErrorMessage);
         return false;
       }
     }
+
     callbackContext.error(action +" is not a valid action");
     return false;
   }
 
-  //Requires at least Marshmallow (API 23)
+  // Requires at least Marshmallow (API 23)
   @TargetApi(Build.VERSION_CODES.M)
   private boolean checkSecureHardware() {
-    message += " using non-legacy method";
     KeyInfo keyInfo = null;
-      boolean authRequired = false;
-    try{
-      // Only for devices with fingerprint reader
-        if (mFingerPrintManager.isHardwareDetected() && mFingerPrintManager.hasEnrolledFingerprints()){
-            authRequired = true
-        }
-      KeyPairGenerator kpg = KeyPairGenerator.getInstance(
-        KeyProperties.KEY_ALGORITHM_RSA,"AndroidKeyStore");
-      kpg.initialize(new KeyGenParameterSpec.Builder(
-        "checkSecureHardware",
-        KeyProperties.PURPOSE_SIGN|KeyProperties.PURPOSE_VERIFY)
-        .setDigests(KeyProperties.DIGEST_SHA256,
-          KeyProperties.DIGEST_SHA512)
-              .setUserAuthenticationRequired(authRequired)
-              .build());
-      KeyPair kp = kpg.generateKeyPair();
-      KeyFactory factory = KeyFactory.getInstance(kp.getPrivate().getAlgorithm(), "AndroidKeyStore");
+    boolean keyInfoGeneratedInSecureHardware = false;
 
-      // Find if key is actually in secure hardware
-      keyInfo = factory.getKeySpec(kp.getPrivate(),KeyInfo.class);
-    }
-    catch (Exception e) {
-      message = Log.getStackTraceString(e);
-    }
-    if (keyInfo != null){
+    String[] priorityListAlgs = {KeyProperties.KEY_ALGORITHM_RSA, KeyProperties.KEY_ALGORITHM_EC};
+
+    for (int i = 0; i < priorityListAlgs.length && !keyInfoGeneratedInSecureHardware; i++) {
       try {
-        // Either condition is true when HW keystore is avaiblable
-        boolean result = (keyInfo.isUserAuthenticationRequirementEnforcedBySecureHardware() || keyInfo.isInsideSecureHardware());
-//        System.out.println("[DBG] keyInfo.isUserAuthenticationRequirementEnforcedBySecureHardware() " + keyInfo.isUserAuthenticationRequirementEnforcedBySecureHardware());
-//        System.out.println("[DBG] keyInfo.isInsideSecureHardware() "+keyInfo.isInsideSecureHardware());
-//        System.out.println("[DBG] KeyChain.isBoundKeyAlgorithm(KeyProperties.KEY_ALGORITHM_EC) "+KeyChain.isBoundKeyAlgorithm(KeyProperties.KEY_ALGORITHM_EC));
-//        System.out.println("[DBG] KeyChain.isBoundKeyAlgorithm(KeyProperties.KEY_ALGORITHM_RSA) "+KeyChain.isBoundKeyAlgorithm(KeyProperties.KEY_ALGORITHM_RSA));
-        KeyStore store = KeyStore.getInstance("AndroidKeyStore");
-        store.load(null, null);
-        store.deleteEntry("checkSecureHardware");
-        // Delete the key after with the alias used
-        return result;
-      } catch (Exception e) {
-        message = Log.getStackTraceString(e);
+        KeyPair kp = generateKeyPair(priorityListAlgs[i]);
+        KeyFactory factory = KeyFactory.getInstance(kp.getPrivate().getAlgorithm(), storeTarget);
+        // Generate dummy key and import it to see if it's in hardware backed Secure Storage
+        keyInfo = factory.getKeySpec(kp.getPrivate(), KeyInfo.class);
+        keyInfoGeneratedInSecureHardware = keyInfo.isInsideSecureHardware();
+      } catch (InvalidKeySpecException | InvalidAlgorithmParameterException | NoSuchProviderException | NoSuchAlgorithmException e) {
+        keyErrorMessage = "Failed to generate dummy key with algorithm: " + priorityListAlgs[i];
+      }
+
+      if (keyInfo != null) {
+        // Delete the dummy key
+        try {
+          KeyStore store = KeyStore.getInstance(storeTarget);
+          store.load(null, null);
+          store.deleteEntry(dummyKeyNamespace);
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
+          keyErrorMessage = "Failed to delete dummy key for algorithm: " + priorityListAlgs[i];
+        }
       }
     }
-    return false;
+
+    return keyInfoGeneratedInSecureHardware;
   }
 
-  //Fallback to legacy/ JellyBean implementation (API 18)
+  @TargetApi(Build.VERSION_CODES.M)
+  private KeyPair generateKeyPair(String keyAlgorithm) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
+
+    KeyPairGenerator kpg = KeyPairGenerator.getInstance(
+            keyAlgorithm, storeTarget);
+
+    kpg.initialize(new KeyGenParameterSpec.Builder(
+            dummyKeyNamespace,
+            KeyProperties.PURPOSE_SIGN|KeyProperties.PURPOSE_VERIFY)
+            .setDigests(KeyProperties.DIGEST_SHA256,
+                    KeyProperties.DIGEST_SHA512)
+            .build());
+
+    return kpg.generateKeyPair();
+  }
+
+  // Fallback to legacy / JellyBean implementation (API 18)
   @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
   private boolean checkSecureHardwareLegacy(){
-    message += " using legacy method";
     // Deprecated for >= API 23
-    return (KeyChain.isBoundKeyAlgorithm(KeyProperties.KEY_ALGORITHM_RSA));
+    return (KeyChain.isBoundKeyAlgorithm(KeyProperties.KEY_ALGORITHM_EC) && KeyChain.isBoundKeyAlgorithm(KeyProperties.KEY_ALGORITHM_RSA));
   }
 }
